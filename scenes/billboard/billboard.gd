0d3d7895aca88905ac495b9c536268f92b594994
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2021-10-02 11:41:35 +0300

    Removed tool from billboard script

--- scenes/billboard/billboard.gd
+++ scenes/billboard/billboard.gd
@@ -1,4 +1,4 @@
-tool
+#tool
 extends Spatial
 
 
@@ -7,8 +7,11 @@ export var label_text = "Example Text" setget _set_label_text, _get_label_text
 onready var label = $Viewport/Label
 
 func _set_label_text(text: String) -> void:
-	label.text = text
+	if label:
+		label.text = text
 
 func _get_label_text() -> String:
-	return label.text
+	if label:
+		return label.text
+	return ""
 
