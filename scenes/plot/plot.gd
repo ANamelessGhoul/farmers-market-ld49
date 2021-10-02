8554a4ae8874ae9322f7987333ca24b036fc4810
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2021-10-02 20:16:28 +0300

    Added crop plot model

--- scenes/plot/plot.gd
+++ scenes/plot/plot.gd
@@ -5,7 +5,7 @@ const CROP_DATA = preload("res://scenes/plot/crop_data.gd")
 
 const MAX_GROWTH: int = 3
 
-export (CROP_DATA.CROPS) var crop_type: int = 0 setget _set_crop_type
+export (CROP_DATA.CROPS) var crop_type: int = 0
 
 var growth_stage: int = 0 setget _set_growth_stage
 var growth_tick: int = 0
@@ -54,7 +54,7 @@ func _set_crop_type(type: int):
 func _clicked():
 	if growth_stage == MAX_GROWTH:
 		harvest()
-		_set_crop_type(CROP_DATA.CROPS.BEET)
+		_set_crop_type(CROP_DATA.CROPS.NONE)
 
 func _on_Area_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
 	if event is InputEventMouseButton:
