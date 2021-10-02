de00fa0d0205d184c949a7d6000d4818d33bf2b5
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2021-10-02 22:28:33 +0300

    Updated crop data and added icons

--- scenes/plot/crop_data.gd
+++ scenes/plot/crop_data.gd
@@ -8,10 +8,10 @@ enum CROPS{
 }
 
 const CROP_DICT: Dictionary = {
-	CROPS.WHEAT: {"animation": "wheat", "growth_ticks": 4},
-	CROPS.BEET: {"animation": "beet", "growth_ticks": 8},
-	CROPS.PUMPKIN: {"animation": "pumpkin", "growth_ticks": 12},
-	CROPS.GRAPE: {"animation": "grape", "growth_ticks": 8},
+	CROPS.WHEAT: {"animation": "wheat", "growth_ticks": 2},
+	CROPS.BEET: {"animation": "beet", "growth_ticks": 4},
+	CROPS.PUMPKIN: {"animation": "pumpkin", "growth_ticks": 6},
+	CROPS.GRAPE: {"animation": "grape", "growth_ticks": 4},
 }
 
 const CROP_VALUES: Dictionary = {
@@ -22,8 +22,15 @@ const CROP_VALUES: Dictionary = {
 }
 
 const CROP_DECAY: Dictionary = {
-	CROPS.WHEAT: 60,
-	CROPS.BEET: 40,
-	CROPS.PUMPKIN: 60,
-	CROPS.GRAPE: 20,
+	CROPS.WHEAT: 30,
+	CROPS.BEET: 20,
+	CROPS.PUMPKIN: 30,
+	CROPS.GRAPE: 15,
+}
+
+const CROP_ICONS: Dictionary = {
+	CROPS.WHEAT: preload("res://sprites/crops/wheat/WheatIcon.png"),
+	CROPS.BEET: null,
+	CROPS.PUMPKIN: null,
+	CROPS.GRAPE: null,
 }
