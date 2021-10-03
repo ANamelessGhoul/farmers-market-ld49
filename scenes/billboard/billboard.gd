e004a2cbc884d062db4a95be28b2df25beed77ee
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2021-10-03 22:32:19 +0300

    Fixed billboard text field

--- scenes/billboard/billboard.gd
+++ scenes/billboard/billboard.gd
@@ -1,4 +1,4 @@
-#tool
+tool
 extends Spatial
 
 
@@ -6,12 +6,16 @@ export var label_text = "Example Text" setget _set_label_text, _get_label_text
 
 onready var label = $Viewport/Label
 
+func _ready():
+	_set_label_text(label_text)
+
 func _set_label_text(text: String) -> void:
+	label_text = text
 	if label:
 		label.text = text
 
 func _get_label_text() -> String:
 	if label:
-		return label.text
+		return label_text
 	return ""
 
