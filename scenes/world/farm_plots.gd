5bb026ac64708557a03bdc1a6a104332b260a8ba
Tecelli Akıntuğ <tecelli.e.a@gmail.com>
2021-10-03 10:37:50 +0300

    Fixed farm plot assignment

--- scenes/world/farm_plots.gd
+++ scenes/world/farm_plots.gd
@@ -9,6 +9,6 @@ func add_crop(crop_type: int) -> bool:
 	for child in get_children():
 		var farm_plot := child as FarmPlot
 		if farm_plot.crop_type == CROP_DATA.CROPS.NONE:
-			farm_plot.crop_type = crop_type
+			farm_plot.set_crop_type(crop_type)
 			return true
 	return false
